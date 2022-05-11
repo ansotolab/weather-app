2a99d8f7d5b9144667198c82e0ce9d9a8ee5fc3a
Antonio Soto <ansotolab@gmail.com>
2022-05-11 11:30:56 +0200

    Test searcher.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="app">
     <el-input placeholder="Madrid..." v-model="city"></el-input>
+    <pre>{{ getCities() }}</pre>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import json from "../cities.json";
+import City from "./entities/city";
 
 @Component({})
 export default class App extends Vue {
   city = "";
+  cities: City[] = json.cities;
+
+  getCities() {
+    if (this.city == "") {
+      return this.cities[1];
+    } else {
+      return this.cities.filter((c: City) =>
+        c.owm_city_name.includes(this.city)
+      );
+    }
+  }
 }
 </script>
