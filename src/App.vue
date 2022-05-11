2ac52473514a634122f6af8b95614a427106beba
Antonio Soto <ansotolab@gmail.com>
2022-05-11 12:12:03 +0200

    City results as cards.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <el-input placeholder="Madrid..." v-model="city"></el-input>
-    <pre>{{ getCities() }}</pre>
+    <CityResult
+      v-for="c in getCities()"
+      :key="c.owm_city_id"
+      :name="c.owm_city_name"
+    ></CityResult>
   </div>
 </template>
 
@@ -9,15 +13,18 @@
 import { Component, Vue } from "vue-property-decorator";
 import json from "../cities.json";
 import City from "./entities/city";
+import CityResult from "./components/CityResult.vue";
 
-@Component({})
+@Component({
+  components: { CityResult },
+})
 export default class App extends Vue {
   city = "";
   cities: City[] = json.cities;
 
   getCities() {
     if (this.city == "") {
-      return this.cities[1];
+      return [this.cities[1]];
     } else {
       return this.cities.filter((c: City) =>
         c.owm_city_name.includes(this.city)
---
+++ src/components/CityResult.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <span>{{ name }}</span>
+    </div>
+  </el-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class CityResult extends Vue {
+  @Prop({ default: "Nombre de la ciudad", required: true })
+  name!: string;
+}
+</script>
