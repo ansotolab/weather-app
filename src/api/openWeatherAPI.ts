7766bf078d6f3dc4bd69251a3cc00570496bc8b9
Antonio Soto <ansotolab@gmail.com>
2022-05-11 17:49:08 +0200

    Change query to metric system.

--- src/api/openWeatherAPI.ts
+++ src/api/openWeatherAPI.ts
@@ -2,6 +2,6 @@ import { WeatherResponse } from "@/entities/oneCallResponse";
 import request from "./baseRequest";
 
 export const getWeather = (lat: string, lon: string) => {
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=5c46b4bfce8cef2f8b453e5827964b54&exclude=minutely,hourly,alerts`;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=5c46b4bfce8cef2f8b453e5827964b54&exclude=minutely,hourly,alerts&units=metric`;
   return request.get<WeatherResponse>(url);
 };
