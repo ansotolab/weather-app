dfeaee862549ae909648e7d1007e4379c0acedbe
Antonio Soto <ansotolab@gmail.com>
2022-05-11 17:22:25 +0200

    Simplify city weather page.

---
+++ src/components/DayWeather.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-col :xs="24" :sm="10" :md="5" :lg="3">
+      <el-card :body-style="{ padding: '0px' }" v-if="day">
+        <img
+          :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+          class="image"
+        />
+        <div style="padding: 14px">
+          <span>Lunes 15</span>
+          <div class="bottom clearfix">
+            <time class="time">12º / 25ª</time>
+          </div>
+        </div>
+      </el-card>
+    </el-col>
+  </div>
+</template>
+
+<script lang="ts">
+import { Daily } from "@/entities/oneCallResponse";
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class DayWeatherPage extends Vue {
+  @Prop({ default: {}, required: true })
+  day!: Daily;
+}
+</script>
--- src/pages/city-weather.vue
+++ src/pages/city-weather.vue
@@ -1,46 +1,44 @@
 <template>
   <div>
-    <h1 style="text-align: center">🌤 El tiempo de alguna ciudad</h1>
+    <h1 style="text-align: center">{{ `🌤 El tiempo de ${name}` }}</h1>
 
-    <pre>{{ latitude }}</pre>
-    <pre>{{ longitude }}</pre>
-
-    <el-row>
-      <el-col :xs="24" :sm="10" :md="5" :lg="3"
-        ><div class="grid-content bg-purple">1</div></el-col
-      >
-      <el-col :xs="24" :sm="10" :md="5" :lg="3"
-        ><div class="grid-content bg-purple-light">2</div></el-col
-      >
-      <el-col :xs="24" :sm="10" :md="5" :lg="3"
-        ><div class="grid-content bg-purple">3</div></el-col
-      >
-      <el-col :xs="24" :sm="10" :md="5" :lg="3"
-        ><div class="grid-content bg-purple-light">4</div></el-col
-      >
-      <el-col :xs="24" :sm="10" :md="5" :lg="3"
-        ><div class="grid-content bg-purple">5</div></el-col
-      >
-      <el-col :xs="24" :sm="10" :md="5" :lg="3"
-        ><div class="grid-content bg-purple-light">6</div></el-col
-      >
-      <el-col :xs="24" :sm="10" :md="5" :lg="3"
-        ><div class="grid-content bg-purple">7</div></el-col
-      >
+    <el-row v-if="!!cityWeather">
+      <DayWeather
+        v-for="(day, index) in cityWeather.daily.slice(0, 7)"
+        :key="index"
+        :day="day"
+      ></DayWeather>
     </el-row>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import { WeatherResponse } from "@/entities/oneCallResponse";
+import { getWeather } from "@/api/openWeatherAPI";
+import DayWeather from "@/components/DayWeather.vue";
 
-@Component({})
+@Component({
+  components: {
+    DayWeather,
+  },
+})
 export default class CityWeatherPage extends Vue {
+  @Prop({ default: "", required: true })
+  name!: string;
+
   @Prop({ default: "", required: true })
   latitude!: string;
 
   @Prop({ default: "", required: true })
   longitude!: string;
+
+  cityWeather: WeatherResponse | null = null;
+
+  async created() {
+    const { data } = await getWeather(this.latitude, this.longitude);
+    this.cityWeather = data;
+  }
 }
 </script>
 
@@ -53,18 +51,5 @@ export default class CityWeatherPage extends Vue {
 .el-col {
   margin: 2px;
   border-radius: 4px;
-}
-.bg-purple-dark {
-  background: #99a9bf;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
-}
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
 }
 </style>
