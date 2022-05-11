cfac86ac6268c3fad80886378d6aee26ae04601a
Antonio Soto <ansotolab@gmail.com>
2022-05-11 17:58:58 +0200

    Add environment variables.

--- src/api/openWeatherAPI.ts
+++ src/api/openWeatherAPI.ts
@@ -2,6 +2,7 @@ import { WeatherResponse } from "@/entities/oneCallResponse";
 import request from "./baseRequest";
 
 export const getWeather = (lat: string, lon: string) => {
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=5c46b4bfce8cef2f8b453e5827964b54&exclude=minutely,hourly,alerts&units=metric`;
+  console.log(process.env);
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.VUE_APP_OPENWEATHER_KEY}&exclude=minutely,hourly,alerts&units=metric`;
   return request.get<WeatherResponse>(url);
 };
