0d249d05032d9c84260c5cc753965ad347616cbe
Antonio Soto <ansotolab@gmail.com>
2022-05-11 14:07:10 +0200

    Seven days weather layout.

--- src/pages/city-weather.vue
+++ src/pages/city-weather.vue
@@ -4,6 +4,30 @@
 
     <pre>{{ latitude }}</pre>
     <pre>{{ longitude }}</pre>
+
+    <el-row>
+      <el-col :xs="24" :sm="10" :md="5" :lg="3"
+        ><div class="grid-content bg-purple">1</div></el-col
+      >
+      <el-col :xs="24" :sm="10" :md="5" :lg="3"
+        ><div class="grid-content bg-purple-light">2</div></el-col
+      >
+      <el-col :xs="24" :sm="10" :md="5" :lg="3"
+        ><div class="grid-content bg-purple">3</div></el-col
+      >
+      <el-col :xs="24" :sm="10" :md="5" :lg="3"
+        ><div class="grid-content bg-purple-light">4</div></el-col
+      >
+      <el-col :xs="24" :sm="10" :md="5" :lg="3"
+        ><div class="grid-content bg-purple">5</div></el-col
+      >
+      <el-col :xs="24" :sm="10" :md="5" :lg="3"
+        ><div class="grid-content bg-purple-light">6</div></el-col
+      >
+      <el-col :xs="24" :sm="10" :md="5" :lg="3"
+        ><div class="grid-content bg-purple">7</div></el-col
+      >
+    </el-row>
   </div>
 </template>
 
@@ -19,3 +43,28 @@ export default class CityWeatherPage extends Vue {
   longitude!: string;
 }
 </script>
+
+<style>
+.el-row {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.el-col {
+  margin: 2px;
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
