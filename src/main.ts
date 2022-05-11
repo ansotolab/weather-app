92ca0969d752db8a6b2554e2b4591c46f248bf48
Antonio Soto <ansotolab@gmail.com>
2022-05-11 13:30:03 +0200

    Add router to main.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Element from "element-ui";
+import router from "./core/router";
 import "element-ui/lib/theme-chalk/index.css";
 import "./assets/index.css";
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false;
 Vue.use(Element);
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
