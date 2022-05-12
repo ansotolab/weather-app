7fdb997da5d1e4316b71d40916c5ed7bdc3121f7
Antonio Soto <ansotolab@gmail.com>
2022-05-12 10:33:18 +0200

    Flag test.

---
+++ tests/unit/flag.spec.ts
@@ -0,0 +1,16 @@
+import { shallowMount } from "@vue/test-utils";
+import Flag from "@/components/Flag.vue";
+
+describe("Flag.vue", () => {
+  it("Renders flag when passed", () => {
+    // Get code with upper and lower letters
+    const isoCode = "Es";
+    const expectedClasses = ["fi", "fi-es", "fis"].toString();
+
+    const wrapper = shallowMount(Flag, {
+      propsData: { isoCode },
+    });
+
+    expect(wrapper.find("span").classes().toString()).toMatch(expectedClasses);
+  });
+});
