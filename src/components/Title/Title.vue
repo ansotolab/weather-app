570fcfd21e34d1287d90be9c6b257ef5cc10630a
Antonio Soto <ansotolab@gmail.com>
2022-05-12 15:50:17 +0200

    Create Title component with I18n.

---
+++ src/components/Title/Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 v-if="city == ''">{{ $t("title.withoutName") }}</h1>
+  <h1 v-else>{{ $t("title.withName", { name: city }) }}</h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class CityResult extends Vue {
+  @Prop({ default: "" })
+  city!: string;
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
